0e4ac782787968578510430caf0d9805f5ee3ce7
Rizki Dwi N <72845868+RizkiDwiN@users.noreply.github.com>
2021-02-20 16:10:06 +0700

    Upload Project UAS

---
+++ src/uas/if2/pkg10119052/rizkidwinugraha/pkg6/UASIF210119052RIZKIDWINUGRAHA6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if2.pkg10119052.rizkidwinugraha.pkg6;
+
+/**
+ * NIM     : 10119052
+ * NAMA    : Rizki Dwi Nugraha
+ * KELAS   : IF-2
+ * TANGGAL : 20 FEBRUARI 2021
+ * NO SOAL : 6
+ */
+public class UASIF210119052RIZKIDWINUGRAHA6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
